461d90b017f424585b7e41995f5c4926a5dd42af
Ho3einNa3iri <MHo3einNa3iri@gmail.com>
2024-10-20 04:34:00 +0330

    all text editions completed and fix some but on custom reserve ,message support,send card info to user , insert delete state on all mark , check be admin in all admin marks

--- auth/auth.py
+++ auth/auth.py
@@ -1,14 +1,13 @@
-# BOT_TOKEN='7174660435:AAHCbo6_bcYq56yJsX8ojTP7-PNLGA6wUgg' # @Test051ybot
-BOT_TOKEN='6457910409:AAFzaPGvGqgRNxg9p6R9uwGDMg5z5i7GxdE' # @saman_test0123_bot
-#BOT_TOKEN='7839461172:AAG6bGt9J71ssSzSjfRLrHu4apxmW5uhY28' #@Angelnailart_bot
+BOT_TOKEN='7174660435:AAHCbo6_bcYq56yJsX8ojTP7-PNLGA6wUgg' # @@test12345beautybot
+#BOT_TOKEN='6457910409:AAFzaPGvGqgRNxg9p6R9uwGDMg5z5i7GxdE' # @saman_test0123_bot
+# BOT_TOKEN='7839461172:AAG6bGt9J71ssSzSjfRLrHu4apxmW5uhY28' #@Angelnailart_bot
 DB_CONFIG={
     'host':'127.0.0.1',
-    'port':'3333',
+    'port':'3306',
     'user':'root',
     'password':'root',
     'database':'beauty_ass'}
 ##############
-SUPPORT_USER_ID=''
-# # MAIN_ADMIN_USER_ID=[1054820423,423977498,316900317]
-# #saman:1054820423
-# # Ho3ein :423977498
+# saman  : 1054820423
+# Ho3ein : 423977498
+# samaneh: 316900317
--- database/db_create_table.py
+++ database/db_create_table.py
@@ -41,22 +41,23 @@ def createTables():
 def insert_basic_setting():
     result=db_bot_setting_get_all()
     if result is None or len(result)<1:
-        db_bot_setting_insert(name="cart",value="6219861934279083")
-        db_bot_setting_insert(name="cart_name",value="سامان یعقوبی")
-        db_bot_setting_insert(name="cart_bank",value="blue")
+        db_bot_setting_insert(name="cart",value="5022291508281118")
+        db_bot_setting_insert(name="cart_name",value="سمانه نصیری")
+        db_bot_setting_insert(name="cart_bank",value="پاسارگاد")
         db_bot_setting_insert(name="bot_is_enable",value="1")
         db_bot_setting_insert(name="main_admin",value="1054820423")
         db_bot_setting_insert(name="welcome_message",value="خوش امدید")
         logging.info("first init info in db_bot_setting is done")
+
     result =db_admin_get_all()
     if result is None or len(result)<1:
-        db_admin_add(admin_id=1054820423,main_admin=True)#saman
-        db_admin_add(admin_id=423977498,main_admin=False)#nasiri
-        logging.info("first init info in db_bot_setting is done")
+        db_admin_add(admin_id=1054820423,main_admin=False)#saman
+        db_admin_add(admin_id=423977498,main_admin=False)#Ho3ein
+        db_admin_add(admin_id=316900317,main_admin=True)#samaneh
+        logging.info("first init info in db_admin_list is done")
     
     #check exist weekly_setting
     result_weekly_setting=db_WeeklySetting_Get_All()
-
     if result_weekly_setting is None:
         db_WeeklySetting_Insert(name='saturday' , value='1' )
         db_WeeklySetting_Insert(name='sunday'   , value='1' )
@@ -67,9 +68,7 @@ def insert_basic_setting():
         db_WeeklySetting_Insert(name='friday'   , value='1' )
         db_WeeklySetting_Insert(name='part1', value='09:00:01/15:00:00')
         db_WeeklySetting_Insert(name='part2', value='15:00:01/20:00:00')
-
-
-    logging.info("db_bot_setting and db_admin_list ,is done before ")
+    logging.info("db_bot_setting and db_weekly_setting,is done before ")
 
 #######################################################################################
 def createUserTable():
